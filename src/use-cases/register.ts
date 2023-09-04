e7b411c3920ed531d5d27020aff368c9be12fce9
Emmanuel Barbosa Monteiro <emmanuel-monteiro@hotmail.com>
2023-09-04 05:25:25 -0300

    test: Added the initial unit test for user creation to verify proper password hashing functionality

--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -1,6 +1,7 @@
 import { UsersRepository } from '@/repositories/users-repository'
 import { hash } from 'bcryptjs'
 import { UserAlreadyExistsError } from './errors/user-already-exists-error'
+import { User } from '@prisma/client'
 
 interface RegisterUseCaseRequest {
     name: string
@@ -8,11 +9,19 @@ interface RegisterUseCaseRequest {
     password: string
 }
 
+interface RegisterUseCaseResponse {
+    user: User
+}
+
 export class RegisterUseCase {
     constructor(private usersRepository: UsersRepository) {}
 
     
-    async execute({ name, email, password }: RegisterUseCaseRequest) {
+    async execute({ 
+        name, 
+        email, 
+        password, 
+    }: RegisterUseCaseRequest): Promise<RegisterUseCaseResponse> {
         const password_hash = await hash(password, 6)
     
         const userWithSameEmail = await this.usersRepository.findByEmail(email)
@@ -21,10 +30,14 @@ export class RegisterUseCase {
             throw new UserAlreadyExistsError()
         }
     
-        await this.usersRepository.create({
+        const user = await this.usersRepository.create({
             name,
             email,
             password_hash
         })
+
+        return {
+            user,
+        }
     }
 }
