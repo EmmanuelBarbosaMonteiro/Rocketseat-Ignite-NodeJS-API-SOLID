ba22ee52e07f3e992ca211b612a6ee30a96aa09e
Emmanuel Barbosa Monteiro <emmanuel-monteiro@hotmail.com>
2023-09-08 07:53:14 -0300

    test and feat: Added in use-case that that user can do check in in different days

--- src/use-cases/check-in.spec.ts
+++ src/use-cases/check-in.spec.ts
@@ -1,14 +1,27 @@
 import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest'
 import { InMemoryCheckInsRepository } from '@/repositories/in-memory/in-memory-check-ins-repository'
 import { CheckinUseCase } from './check-in'
+import { InMemoryGymsRepository } from '@/repositories/in-memory/in-memory-gyms-repository'
+import { Decimal } from '@prisma/client/runtime/library'
 
 let checkInsRepository: InMemoryCheckInsRepository
+let gymsRepository: InMemoryGymsRepository
 let sut: CheckinUseCase
 
 describe('Check-in Use Case', () => {
     beforeEach(() => {
         checkInsRepository = new InMemoryCheckInsRepository()
-        sut = new CheckinUseCase(checkInsRepository)
+        gymsRepository = new InMemoryGymsRepository()
+        sut = new CheckinUseCase(checkInsRepository, gymsRepository)
+
+        gymsRepository.items.push({
+            id: 'gym-01',
+            title: 'JavaScript Gym',
+            description:'',
+            phone: '',
+            latitude: new Decimal(0),
+            longitude: new Decimal(0),
+        })
 
         vi.useFakeTimers()
     })
@@ -21,6 +34,8 @@ describe('Check-in Use Case', () => {
         const { checkIn } = await sut.execute({
             gymId: 'gym-01',
             userId: 'user-01',
+            userLatitude: -27.2092052,
+            userLongitude: -49.6401091,
         })
         
         expect(checkIn.id).toEqual(expect.any(String))
@@ -32,12 +47,16 @@ describe('Check-in Use Case', () => {
         await sut.execute({
             gymId: 'gym-01',
             userId: 'user-01',
+            userLatitude: -27.2092052,
+            userLongitude: -49.6401091,
         })
         
         await expect(() => 
             sut.execute({
                 gymId: 'gym-01',
                 userId: 'user-01',
+                userLatitude: -27.2092052,
+                userLongitude: -49.6401091,
             })
         
         ).rejects.toBeInstanceOf(Error)
@@ -49,6 +68,8 @@ describe('Check-in Use Case', () => {
         await sut.execute({
             gymId: 'gym-01',
             userId: 'user-01',
+            userLatitude: -27.2092052,
+            userLongitude: -49.6401091,
         })
 
         vi.setSystemTime(new Date(2022, 0, 21, 8, 0, 0))
@@ -56,6 +77,8 @@ describe('Check-in Use Case', () => {
         const {checkIn} = await sut.execute({
                 gymId: 'gym-01',
                 userId: 'user-01',
+                userLatitude: -27.2092052,
+                userLongitude: -49.6401091,
             })
         expect(checkIn.id).toEqual(expect.any(String))
     }) 
--- src/use-cases/check-in.ts
+++ src/use-cases/check-in.ts
@@ -1,9 +1,13 @@
 import { CheckIn } from '@prisma/client'
 import { CheckInsRepository } from '@/repositories/check-ins-repository'
+import { GymsRepository } from '@/repositories/gyms-repository'
+import { ResourceNotFoundError } from './errors/resource-not-found-error'
 
 interface CheckinUseCaseRequest {
     userId: string,
-    gymId: string
+    gymId: string,
+    userLatitude: number,
+    userLongitude: number
 }
 
 interface CheckinUseCaseResponse {
@@ -11,12 +15,21 @@ interface CheckinUseCaseResponse {
 }
 
 export class CheckinUseCase {
-    constructor(private checkInsRepository: CheckInsRepository) {}
+    constructor(
+        private checkInsRepository: CheckInsRepository,
+        private gymsRepository: GymsRepository,
+        ) {}
 
     async execute({ 
         userId,
         gymId,
     }: CheckinUseCaseRequest): Promise<CheckinUseCaseResponse> {
+        const gym = await this.gymsRepository.findById(gymId)
+
+        if(!gym) {
+            throw new ResourceNotFoundError()
+        }
+
         const checkInOnSameDate = await this.checkInsRepository.findByUserIdOneDate(
             userId,
             new Date(),
