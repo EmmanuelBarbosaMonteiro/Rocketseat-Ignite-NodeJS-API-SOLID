79f7ccd0c37c86a112be7d6141bd55989abc3135
Emmanuel Barbosa Monteiro <emmanuel-monteiro@hotmail.com>
2023-09-11 05:00:10 -0300

    feat: Added use-case to search for gyms

---
+++ src/use-cases/search-gyms.ts
@@ -0,0 +1,29 @@
+import { Gym } from '@prisma/client'
+import { GymsRepository } from '@/repositories/gyms-repository'
+
+interface SearchGymsUseCaseRequest {
+    query: string
+    page: number
+}
+
+interface SearchGymsUseCaseResponse {
+    gyms: Gym[]
+}
+
+export class CreateGymCaseUse {
+    constructor(private gymRepository: GymsRepository) {}
+    
+    async execute({ 
+        query,
+        page
+    }: SearchGymsUseCaseRequest): Promise<SearchGymsUseCaseResponse> {
+        const gyms = await this.gymRepository.searchMany(
+            query,
+            page,
+        )
+
+        return {
+            gyms,
+        }
+    }
+}
