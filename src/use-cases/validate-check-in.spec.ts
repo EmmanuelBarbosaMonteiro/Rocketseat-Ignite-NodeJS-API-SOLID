f3265f07fb5be9b1c1fb0d8e092353652cac974a
Emmanuel Barbosa Monteiro <emmanuel-monteiro@hotmail.com>
2023-09-19 04:44:54 -0300

    test: Added unit test for checking in validation within 20 minutes of creation

--- src/use-cases/validate-check-in.spec.ts
+++ src/use-cases/validate-check-in.spec.ts
@@ -11,11 +11,11 @@ describe('Validate Check-in Use Case', () => {
     checkInsRepository = new InMemoryCheckInsRepository()
     sut = new ValidateCheckInUseCase(checkInsRepository)
 
-    // vi.useFakeTimers()
+    vi.useFakeTimers()
   })
 
   afterEach(() => {
-    // vi.useRealTimers()
+    vi.useRealTimers()
   })
 
   it('should be able to validate check-in', async () => {
@@ -38,5 +38,23 @@ describe('Validate Check-in Use Case', () => {
         checkInId: 'inexistent-check-in-id',
       }),
     ).rejects.toBeInstanceOf(ResourceNotFoundError)
+  })
+
+  it('should not be albe to validate the check-in after 20 minutes of its creation', async () =>{
+    vi.setSystemTime(new Date(2023, 0, 1, 13, 40))
+
+    const createdCheckIn = await checkInsRepository.create({
+      gym_id: 'gym-01',
+      user_id: 'user-01',
+    })
+
+    const twentyOneMinutesInMs = 1000 * 60 * 21
+
+    vi.advanceTimersByTime(twentyOneMinutesInMs)
+
+    await expect(() => 
+    sut.execute({
+      checkInId: createdCheckIn.id,
+    })).rejects.toBeInstanceOf(Error) 
   })
 })
