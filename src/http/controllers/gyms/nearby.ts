071c9d31114cea08a71ae1964130bb1585c1edcc
Emmanuel Barbosa Monteiro <emmanuel-monteiro@hotmail.com>
2023-09-26 07:22:51 -0300

    feat: Added gym resource controllers (search, nearby) and routes (create, search, nearby)

---
+++ src/http/controllers/gyms/nearby.ts
@@ -0,0 +1,27 @@
+import { z } from 'zod'
+import { FastifyRequest, FastifyReply } from 'fastify'
+import { makeFetchNearbyGymsUseCase } from '@/use-cases/factories/make-fetch-nearby-gyms-use-case'
+
+export async function nearby(request: FastifyRequest, reply: FastifyReply) {
+  const nearbyGymQuerySchema = z.object({
+    latitude: z.number().refine((value) => {
+      return Math.abs(value) <= 90
+    }),
+    longitude: z.number().refine((value) => {
+      return Math.abs(value) <= 180
+    }),
+  })
+
+  const { latitude, longitude } = nearbyGymQuerySchema.parse(request.body)
+
+  const fetchNearbyGymsUseCase = makeFetchNearbyGymsUseCase()
+
+  const { gyms } = await fetchNearbyGymsUseCase.execute({
+    userLatitude: latitude,
+    userLongitude: longitude,
+  })
+
+  return reply.status(200).send({
+    gyms,
+  })
+}
