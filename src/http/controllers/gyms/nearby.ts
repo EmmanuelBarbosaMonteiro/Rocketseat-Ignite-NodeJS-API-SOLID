fd1459bffee612249a7fb28042c51e7166ebb587
Emmanuel Barbosa Monteiro <emmanuel-monteiro@hotmail.com>
2023-09-26 08:01:35 -0300

    feat: Added check-in create controller and route

--- src/http/controllers/gyms/nearby.ts
+++ src/http/controllers/gyms/nearby.ts
@@ -12,7 +12,7 @@ export async function nearby(request: FastifyRequest, reply: FastifyReply) {
     }),
   })
 
-  const { latitude, longitude } = nearbyGymQuerySchema.parse(request.body)
+  const { latitude, longitude } = nearbyGymQuerySchema.parse(request.query)
 
   const fetchNearbyGymsUseCase = makeFetchNearbyGymsUseCase()
 
