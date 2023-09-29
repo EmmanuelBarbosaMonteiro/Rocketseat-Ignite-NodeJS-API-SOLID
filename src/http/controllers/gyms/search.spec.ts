ee088d75a0913cf3f717e512f59f0de45e994bfe
Emmanuel Barbosa Monteiro <emmanuel-monteiro@hotmail.com>
2023-09-29 06:22:59 -0300

    test: Added test e2e for search gym

---
+++ src/http/controllers/gyms/search.spec.ts
@@ -0,0 +1,55 @@
+import request from 'supertest'
+import { app } from '@/app'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+import { createAndAuthenticateUser } from '@/utils/test/create-and-authenticate-use'
+
+describe('Search Gym (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to create a gym', async () => {
+    const { token } = await createAndAuthenticateUser(app)
+
+    await request(app.server)
+      .post('/gyms')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        title: 'JavaScript Gym',
+        description: 'Some description',
+        phone: '31999999999',
+        latitude: -27.2092052,
+        longitude: -49.6401091,
+      })
+
+    await request(app.server)
+      .post('/gyms')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        title: 'TypeScript Gym',
+        description: 'Some description',
+        phone: '31999999999',
+        latitude: -27.2092052,
+        longitude: -49.6401091,
+      })
+
+    const response = await request(app.server)
+      .get('/gyms/search')
+      .query({
+        query: 'JavaScript',
+      })
+      .set('Authorization', `Bearer ${token}`)
+
+    expect(response.statusCode).toEqual(200)
+    expect(response.body.gyms).toHaveLength(1)
+    expect(response.body.gyms).toEqual([
+      expect.objectContaining({
+        title: 'JavaScript Gym',
+      }),
+    ])
+  })
+})
